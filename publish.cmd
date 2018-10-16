7f4836d54f76bdccc9f3d56a1548f74ee1b00397
Carlos Landaverde <landavercarlos459@outlook.com>
2018-10-16 10:44:24 -0600

    primer commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=botluiscgc-Web-Deploy.pubxml -p:Password=8fgf0wM3kllalDglk9Kr0nWmph6QFD4EMtZembgnweDtZt8LDSft3Ethjipe
+
